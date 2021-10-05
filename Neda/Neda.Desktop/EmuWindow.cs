b889c5c1b941a02c237681854582550a1a9fd50a
xafero <xafero@outlook.com>
2021-10-05 05:42:58 +0200

    Adding missing code

---
+++ Neda/Neda.Desktop/EmuWindow.cs
@@ -0,0 +1,17 @@
+﻿using Cairo;
+using Gtk;
+
+namespace Neda.Desktop
+{
+	internal class EmuWindow : DrawingArea
+	{
+		public EmuWindow()
+		{
+		}
+
+		protected override bool OnDrawn(Context g)
+		{
+			return false; // TODO
+		}
+	}
+}
