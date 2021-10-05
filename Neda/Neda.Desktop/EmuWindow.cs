949c7f46211ef1d990de6b443c4e95f5d96ae1e6
xafero <xafero@outlook.com>
2021-10-05 15:22:27 +0200

    Added menu bar and core console

--- Neda/Neda.Desktop/EmuWindow.cs
+++ Neda/Neda.Desktop/EmuWindow.cs
@@ -1,17 +1,61 @@
-﻿using Cairo;
+﻿using System;
+using System.Text;
+using Cairo;
 using Gtk;
+using Context = Cairo.Context;
 
 namespace Neda.Desktop
 {
 	internal class EmuWindow : DrawingArea
 	{
+		private readonly int _cols;
+		private readonly int _rows;
+		private readonly char[] _screen;
+
 		public EmuWindow()
 		{
+			_cols = 80;
+			_rows = 25;
+			_screen = new char[_cols * _rows];
 		}
 
 		protected override bool OnDrawn(Context g)
 		{
-			return false; // TODO
+			var width = AllocatedWidth;
+			var height = AllocatedHeight;
+
+			g.SetSourceRGB(0, 0, 0);
+			g.Rectangle(0, 0, width, height);
+			g.Fill();
+
+			var wFactor = width / (Program.Width * 1d);
+			var hFactor = height / (Program.Height * 1d);
+			var factor = Math.Min(wFactor, hFactor);
+
+			g.Scale(factor, factor);
+
+			const int xShift = 10;
+			const int yShift = 20;
+			const int ySize = 10;
+
+			g.SelectFontFace("Courier New", FontSlant.Normal, FontWeight.Bold);
+			g.SetFontSize(13);
+
+			for (var y = 0; y < _rows; y++)
+			{
+				var bld = new StringBuilder();
+				for (var x = 0; x < _cols; x++)
+				{
+					var arrayIndex = y * _cols + x;
+					var letter = _screen[arrayIndex];
+					bld.Append(letter);
+				}
+				g.SetSourceRGB(255, 255, 255);
+				g.MoveTo(xShift, yShift + (ySize * y));
+				g.ShowText(bld.ToString());
+			}
+
+			return false;
 		}
 	}
 }
--- Neda/Neda.Desktop/Program.cs
+++ Neda/Neda.Desktop/Program.cs
@@ -7,8 +7,8 @@ namespace Neda.Desktop
 {
 	internal static class Program
 	{
-		private static readonly int width = 640;
-		private static readonly int height = 480;
+		internal static readonly int Width = 645;
+		internal static readonly int Height = 280;
 
 		private static void Main(string[] _)
 		{
@@ -17,21 +17,35 @@ namespace Neda.Desktop
 			var gtk = new EmuWindow();
 
 			var frame = new Window("Neda Desktop");
-			frame.DeleteEvent += delegate
-			{
-				Application.Quit();
-				Environment.Exit(0);
-			};
-			frame.SetDefaultSize((int)(width * 1.5), (int)(height * 1.5));
+			frame.DeleteEvent += DoQuit;
+			frame.SetDefaultSize((int)(Width * 1.5), (int)(Height * 1.5));
 			frame.SetPosition(WindowPosition.Center);
+
+			var mb = new MenuBar();
+			var fileMenu = new Menu();
+			var file = new MenuItem("File") { Submenu = fileMenu };
+			var exit = new MenuItem("Exit");
+			exit.Activated += DoQuit;
+			fileMenu.Append(exit);
+			mb.Append(file);
+
+			var box = new VBox(false, 2);
+			box.PackStart(mb, false, false, 0);
+			box.Add(gtk);
 
 			var icon = Resources.FindResource("/icons/app.png");
 			frame.Icon = new Pixbuf(icon);
 
-			frame.Add(gtk);
+			frame.Add(box);
 			frame.ShowAll();
 
 			Application.Run();
+		}
+
+		private static void DoQuit<T>(object o, T args) where T : EventArgs
+		{
+			Application.Quit();
+			Environment.Exit(0);
 		}
 	}
 }
