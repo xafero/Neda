1bf17ee5a3efca4773768ff0723ce7b47a916b5f
xafero <gberendt@outlook.com>
2021-10-06 11:04:19 +0200

    ReadLine works

--- Neda/Neda.Desktop/EmuWindow.cs
+++ Neda/Neda.Desktop/EmuWindow.cs
@@ -2,82 +2,116 @@
 using System.Text;
 using System.Threading;
 using Cairo;
+using Gdk;
 using Gtk;
 using Neda.API;
 using Context = Cairo.Context;
 
 namespace Neda.Desktop
 {
-	internal class EmuWindow : DrawingArea, IConsole
-	{
-		private readonly int _cols;
-		private readonly int _rows;
-		private readonly char[] _screen;
+    internal class EmuWindow : DrawingArea, IConsole
+    {
+        private readonly int _cols;
+        private readonly int _rows;
+        private readonly char[] _screen;
 
-		public EmuWindow()
-		{
-			_cols = 80;
-			_rows = 25;
-			_screen = new char[_cols * _rows];
-		}
+        public EmuWindow()
+        {
+            _cols = 80;
+            _rows = 25;
+            _screen = new char[_cols * _rows];
+        }
 
-		public void Clear()
-		{
-			for (var i = 0; i < _screen.Length; i++)
-				_screen[i] = default;
-			Refresh(this);
-		}
+        public void Clear()
+        {
+            for (var i = 0; i < _screen.Length; i++)
+                _screen[i] = default;
+            Refresh(this);
+        }
 
-		public void Write(string text)
-		{
-			for (var i = 0; i < text.Length; i++)
-				_screen[i] = text[i];
-			Refresh(this);
-		}
+        public void Write(string text)
+        {
+            for (var i = 0; i < text.Length; i++)
+                _screen[i] = text[i];
+            Refresh(this);
+        }
 
-		public void WriteLine(string text)
-			=> Write(text + Environment.NewLine);
+        public void WriteLine(string text)
+            => Write(text + Environment.NewLine);
 
-		private static void Refresh(Widget widget)
-			=> Application.Invoke((o, e) => widget.QueueDraw());
+        private static void Refresh(Widget widget)
+            => Application.Invoke((o, e) => widget.QueueDraw());
 
-		protected override bool OnDrawn(Context g)
-		{
-			var width = AllocatedWidth;
-			var height = AllocatedHeight;
+        protected override bool OnDrawn(Context g)
+        {
+            var width = AllocatedWidth;
+            var height = AllocatedHeight;
 
-			g.SetSourceRGB(0, 0, 0);
-			g.Rectangle(0, 0, width, height);
-			g.Fill();
+            g.SetSourceRGB(0, 0, 0);
+            g.Rectangle(0, 0, width, height);
+            g.Fill();
 
-			var wFactor = width / (Program.Width * 1d);
-			var hFactor = height / (Program.Height * 1d);
-			var factor = Math.Min(wFactor, hFactor);
+            var wFactor = width / (Program.Width * 1d);
+            var hFactor = height / (Program.Height * 1d);
+            var factor = Math.Min(wFactor, hFactor);
 
-			g.Scale(factor, factor);
+            g.Scale(factor, factor);
 
-			const int xShift = 10;
-			const int yShift = 20;
-			const int ySize = 10;
+            const int xShift = 10;
+            const int yShift = 20;
+            const int ySize = 10;
 
-			g.SelectFontFace("Courier New", FontSlant.Normal, FontWeight.Bold);
-			g.SetFontSize(13);
+            g.SelectFontFace("Courier New", FontSlant.Normal, FontWeight.Bold);
+            g.SetFontSize(13);
 
-			for (var y = 0; y < _rows; y++)
-			{
-				var bld = new StringBuilder();
-				for (var x = 0; x < _cols; x++)
-				{
-					var arrayIndex = y * _cols + x;
-					var letter = _screen[arrayIndex];
-					bld.Append(letter);
-				}
-				g.SetSourceRGB(255, 255, 255);
-				g.MoveTo(xShift, yShift + (ySize * y));
-				g.ShowText(bld.ToString());
-			}
+            for (var y = 0; y < _rows; y++)
+            {
+                var bld = new StringBuilder();
+                for (var x = 0; x < _cols; x++)
+                {
+                    var arrayIndex = y * _cols + x;
+                    var letter = _screen[arrayIndex];
+                    bld.Append(letter);
+                }
+                g.SetSourceRGB(255, 255, 255);
+                g.MoveTo(xShift, yShift + (ySize * y));
+                g.ShowText(bld.ToString());
+            }
 
-			return false;
-		}
-	}
+            return false;
+        }
+
+        private readonly StringBuilder _currentLine = new StringBuilder();
+        private readonly ManualResetEvent _nextLineEvent = new ManualResetEvent(false);
+
+        protected override bool OnKeyPressEvent(EventKey e)
+        {
+            var letter = (char)Gdk.Keyval.ToUnicode(e.KeyValue);
+            if (letter == default)
+            {
+                Console.Error.WriteLine("Special key ?! " + e.Key);
+            }
+            else
+            {
+                if (e.Key == Gdk.Key.Return || e.Key == Gdk.Key.KP_Enter)
+                {
+                    _nextLineEvent.Set();
+                }
+                else
+                {
+                    _currentLine.Append(letter);
+                }
+            }
+            return base.OnKeyPressEvent(e);
+        }
+
+        public string ReadLine()
+        {
+            _nextLineEvent.WaitOne();
+            _nextLineEvent.Reset();
+            var line = _currentLine.ToString();
+            _currentLine.Clear();
+            return line;
+        }
+    }
 }
